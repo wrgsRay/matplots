94b55f09359c1ff584c4f07751b7299ee636c898
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-12 17:04:09 -0700

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,15 @@
+"""
+Python 3.6
+@Author: wrgsRay
+"""
+import matplotlib.pyplot as plt
+
+
+def main():
+    sqaures = [1, 4, 9, 16, 35]
+    plt.plot(sqaures)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
