2f17b924b06bd7d6316b9162d201c0ddb48d7e89
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-14 16:02:18 -0700

    finished

--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def main():
     x_values = [x for x in range(1, 1001)]
     y_values = [x**2 for x in x_values]
-    plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=10)
+    plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Purples, edgecolor='none', s=10)
 
     # Set chart title and label axes.
     plt.title('Square Numbers', fontsize=24)
@@ -20,6 +20,9 @@ def main():
 
     # Set the range for each axis.
     plt.axis([0, 1100, 0, 1100000])
+
+    # Save file
+    # plt.savefig(f'{__name__}.png', bbox_inches='tight')
 
     plt.show()
 
