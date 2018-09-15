c6cf94b209bdeba6e6905235d9518f15752153e2
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-14 17:17:22 -0700

    random walked

--- rw_visual.py
+++ rw_visual.py
@@ -9,10 +9,21 @@ from random_walk import RandomWalk
 def main():
     # Keep making new waalks, as long as the program is active
     while True:
-        rw = RandomWalk()
+        rw = RandomWalk(50000)
         rw.fill_walk()
 
-        plt.scatter(rw.x_values, rw.y_values, s=1)
+        plt.figure(figsize=(10, 6))
+        point_numbers = list(range(rw.num_points))
+
+        # plt.plot(rw.x_values, rw.y_values, linewidth=1)
+        plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Purples, edgecolor='none', s=1)
+        # Emphasize the first and last points.
+        plt.scatter(0, 0, c='green', edgecolors='none', s=50)
+        plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
+
+        # Remove the axes.
+        plt.axes().get_xaxis().set_visible(False)
+        plt.axes().get_yaxis().set_visible(False)
         plt.show()
 
         keep_running = input('Make another walk? (y/n):')
