01f17e9311b9ae8e7d5ec64d087f5c668156e55a
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-14 17:09:59 -0700

    refactor

--- random_walk.py
+++ random_walk.py
@@ -22,13 +22,9 @@ class RandomWalk:
         # Keep taking steps until the walk reaches desired length.
         while len(self.x_values) < self.num_points:
             # Decide which direction to go and how far to go in that direction.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
@@ -40,6 +36,12 @@ class RandomWalk:
 
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+
+    @staticmethod
+    def get_step():
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
 
 
 def main():
