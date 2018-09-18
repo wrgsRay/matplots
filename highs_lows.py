dba38f0a29f163d7bfa4d401f0c3ad7fe395dc2e
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-18 16:57:41 -0700

    a year's data

--- highs_lows.py
+++ highs_lows.py
@@ -3,30 +3,35 @@ Python 3.6
 @Author: wrgsRay
 """
 import csv
+from datetime import datetime
 from matplotlib import pyplot as plt
 
 
 def main():
-    filename = 'sitka_weather_07-2014.csv'
+    filename = 'sitka_weather_2014.csv'
     with open(filename) as f:
         reader = csv.reader(f)
         header_row = next(reader)
 
         # for index, column_header in enumerate(header_row):
         #    print(index, column_header)
+        dates = list()
         highs = list()
         for row in reader:
+            current_date = datetime.strptime(row[0], '%Y-%m-%d')
+            dates.append(current_date)
             high = int(row[1])
             highs.append(high)
 
 
     # Plot data
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(highs, c='red')
+    plt.plot(dates, highs, c='red')
 
     # Format plot
-    plt.title('Daily high temperatures, July 2014')
+    plt.title('Daily high temperatures 2014', fontsize=24)
     plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
     plt.ylabel('Temperature (F)', fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
 
