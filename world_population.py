7100b649457323991278e5720de2ad34ffad0007
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-20 16:58:05 -0700

    extracting data from json

---
+++ world_population.py
@@ -0,0 +1,22 @@
+"""
+Python 3.6
+@Author: wrgsRay
+"""
+import json
+
+
+def main():
+    # load the data into a list.
+    filename = 'population_data.json'
+    with open(filename) as f:
+        pop_data = json.load(f)
+    # Print the 2010 population for each country.
+    for pop_dict in pop_data:
+        if pop_dict['Year'] == '2010':
+            country_name = pop_dict['Country Name']
+            population = pop_dict['Value']
+            print(f'{country_name} : {population}')
+
+
+if __name__ == '__main__':
+    main()
